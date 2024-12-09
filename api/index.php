ebb00b248cb27c457ae407cefb3cc287e0bef299
Untitled-developerYT <marn74678@gmail.com>
2024-12-09 18:19:13 +0000

    Update index.php

--- api/index.php
+++ api/index.php
@@ -153,7 +153,7 @@ if (isset($_GET['action'])) {
 <body>
 
 <div class="container">
-
+<iframe src="https://discordapp.com/widget?id=1277599930621366312&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
 <form method="POST">
         <label for="botToken">Bot Token:</label><br>
         <input type="password" id="botToken" name="botToken" value="<?= htmlspecialchars($botToken) ?>" required><br><br>
